bd45fcc4cb84331998938b9c7280c82c7545aa43
Ved Prakash Bhaskar <iamved99@gmail.com>
2022-07-03 01:02:45 +0530

    react app pushed

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Header = ({ handleToggleDarkMode }) => {
+	return (
+		<div className='header'>
+			<h1>Insights</h1>
+			<button
+				onClick={() =>
+					handleToggleDarkMode(
+						(previousDarkMode) => !previousDarkMode
+					)
+				}
+				className='save'
+			>
+				Theme Mode
+			</button>
+		</div>
+	);
+};
+
+export default Header;
