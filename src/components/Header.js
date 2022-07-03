1fd6306716eb7ccbb97b322bf9618129f0baa9a0
Ved Prakash Bhaskar <iamved99@gmail.com>
2022-07-03 09:14:13 +0530

    changes done in index

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = ({ handleToggleDarkMode }) => {
 	return (
 		<div className='header'>
-			<h1>Insights - Notes Hub</h1>
+			<h1>Insights</h1>
 			<button
 				onClick={() =>
 					handleToggleDarkMode(
